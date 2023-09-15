8a9f7a2bd3033eca36ca71aafcd600192a81ea6e
Jin <jin.her@edu.sait.ca>
2023-09-15 20:54:22 +0100

    Added StudentInfo Component

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <div>
+        <p>Jin Her</p>
+        <p>CPRG-306-A</p>
+        <Link href="https://github.com/JinPlanter/cprg306-assignments">JinHer - GitHub Repo</Link>
+      </div>
+    );
+  }
