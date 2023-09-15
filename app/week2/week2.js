0442b1b0c44deff8d98febecb53bee4b71049eb8
Jin <jin.her@edu.sait.ca>
2023-09-15 21:00:59 +0100

    Added week2 page

---
+++ app/week2/week2.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import StudentInfo from './StudentInfo'
+
+export default function Page() {
+  return (
+    <main>
+      <StudentInfo />
+      <h1>My Shopping List</h1>
+    </main>
+  );
+}
