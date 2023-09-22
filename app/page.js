a15f9d2f585921563f30077088fe509f180f97e1
Jin <jin.her@edu.sait.ca>
2023-09-22 13:37:19 +0100

    Remove line of unnecessary code

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import StudentInfo from './StudentInfo'
-import Week2 from './week2/week2'
 
 export default function Page() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
-      <Week2 />
     </main>
   );
 }
